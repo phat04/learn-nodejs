e2dbf4860786b0408de362148e82e9b44e7db0ae
Phat04 <ghjnnii@gmail.com>
2022-09-02 13:19:42 +0700

    Please enter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -1,37 +1,11 @@
-const { readFile, writeFile } = require("fs").promisesf;
-const util = require("util");
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
+const EventEmitter = require("events");
+const customEmitter = new EventEmitter();
+customEmitter.on("reponse", (name, id) => {
+  console.log(`data recieved ${name} with id:${id}`);
+});
 
-const start = async () => {
-  try {
-    const first = await readFile("./content/first.txt", "utf8");
-    const second = await readFile("./content/second.txt", "utf8");
-    await writeFilePromise(
-      "./content/result-mind-grernade.txt",
-      `this is awef: ${first} ${second}`
-    );
-    console.log(first, second);
-    // console.log(second);
-  } catch (error) {
-    console.log(error);
-  }
-};
+customEmitter.on("reponse", () => {
+  console.log(`The other logic here`);
+});
 
-start();
-
-// getText("./content/first.txt")
-//   .then((result) => console.log(result))
-//   .catch((err) => console.log(err));
-
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, "utf8", (err, data) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         console.log(resolve(data));
-//       }
-//     });
-//   });
-// };
+customEmitter.emit("reponse", "Phat", 44);
